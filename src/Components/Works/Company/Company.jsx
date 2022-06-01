857849d0d3814737f503f50d82a452252365713c
Krishna <krishnabiswakarmasc@gmail.com>
2022-06-02 01:06:19 +0530

    Added the new internship and many more things are adjusted accordingly

---
+++ src/Components/Works/Company/Company.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Company.css";
+import { motion } from "framer-motion";
+// import ReactJS from "@iconscout/react-unicons/icons/uil-react";
+
+const Company = (props) => {
+  const Skill1 = props.skill1;
+  const Skill2 = props.skill2;
+  const Skill3 = props.skill3;
+  const Skill4 = props.skill4;
+  const Skill5 = props.skill5;
+  return (
+    <div className="works" id="Internship">
+      <div className="awesome internship">
+        {/* <span style={{ color: darkMode ? "white" : "" }}>Internship</span>
+        <span>Experience</span> */}
+        <span></span>
+        <span></span>
+        <span>
+          COMPANY NAME -{" "}
+          <span>
+            <a href={props.link}>{props.cName}</a>
+          </span>
+        </span>
+        <ul className="w-details">
+          <li>{props.list1}</li>
+          <br />
+          <li>{props.list2}</li>
+          <br />
+          <li>{props.list3}</li>
+          <br />
+          <li>{props.list4}</li>
+          <br />
+          <li>{props.list5}</li>
+        </ul>
+        {/* <button className="button s-button">Hire Me</button> */}
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      <div className="w-right">
+        <motion.div
+          initial={{ rotate: 45 }}
+          whileInView={{ rotate: 0 }}
+          viewport={{ margin: "-40px" }}
+          transition={{ duration: 3.5, type: "spring" }}
+          className="w-mainCircle"
+        >
+          <div className="w-secCircle" style={{ background: "#00d8ff" }}>
+            <Skill1 color="white" size="4rem" />
+          </div>
+          <div className="w-secCircle" style={{ background: "#D7492F" }}>
+            <img src={Skill2} alt="" width="500%" height="500%" />
+          </div>
+          <div className="w-secCircle" style={{ background: "#F7E04B" }}>
+            <Skill3
+              color="black"
+              size="4rem"
+              style={{ background: "#F7E04B" }}
+            />
+          </div>
+          <div className="w-secCircle" style={{ background: "#2D394A" }}>
+            <img src={Skill4} alt="" />
+          </div>
+          <div className="w-secCircle" style={{ background: "#2677BD" }}>
+            <img src={Skill5} alt="" width="500%" height="500%" />
+          </div>
+        </motion.div>
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Company;
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,65 +1,56 @@
 import React, { useContext } from "react";
 import "./Works.css";
 import { themeContext } from "../../Context";
-import { motion } from "framer-motion";
+import Company from "./Company/Company";
+
+// Company 1
 import ReactJS from "@iconscout/react-unicons/icons/uil-react";
-import HTML from "@iconscout/react-unicons/icons/uil-html5";
 import JS from "@iconscout/react-unicons/icons/uil-java-script";
-import CSS from "@iconscout/react-unicons/icons/uil-css3-simple";
 import Bootstrap from "../../img/bootstrap.png";
+import CSS from "../../img/css.png";
+import HTML from "../../img/html.png";
+
+// Company 2
+import Tailwind from "../../img/tailwind.png";
+import TS from "../../img/ts.png";
+
+// import TS from "../../img/ts.png";
 
 const Works = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
-    <div className="works" id="Internship">
-      <div className="awesome internship">
-        <span style={{ color: darkMode ? "white" : "" }}>Internship</span>
-        <span>Experience</span>
-        <span>
-          Company Name - <span>DigitalBoxItIn</span>
-        </span>
-        <ul className="w-details">
-          <li>Responsible for handling the Front-End of the Wesbite.</li>
-          <br />
-          <li>Tech Stack used HTML, CSS, JS, ReactJS.</li>
-          <br />
-          <li>Framework used Bootstrap and MaterialUI.</li>
-          <br />
-          <li>Learnt to Work in a Team.</li>
-          <br />
-          <li>Learnt Time Management.</li>
-        </ul>
-        {/* <button className="button s-button">Hire Me</button> */}
-        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
-      </div>
-      <div className="w-right">
-        <motion.div
-          initial={{ rotate: 45 }}
-          whileInView={{ rotate: 0 }}
-          viewport={{ margin: "-40px" }}
-          transition={{ duration: 3.5, type: "spring" }}
-          className="w-mainCircle"
-        >
-          <div className="w-secCircle" style={{ background: "#00d8ff" }}>
-            <ReactJS color="white" size="3rem" />
-          </div>
-          <div className="w-secCircle">
-            <HTML color="black" size="3rem" style={{ background: "#D7492F" }} />
-          </div>
-          <div className="w-secCircle" style={{ background: "#F7E04B" }}>
-            <JS color="black" size="3rem" />
-          </div>
-          <div className="w-secCircle" style={{ background: "#2D394A" }}>
-            <img src={Bootstrap} alt="" />
-          </div>
-          <div className="w-secCircle" style={{ background: "#264BDD" }}>
-            <CSS color="black" size="3rem" />
-          </div>
-        </motion.div>
-        <div className="w-backCircle blueCircle"></div>
-        <div className="w-backCircle yellowCircle"></div>
-      </div>
+    <div className="awesome w-wrapper">
+      <span style={{ color: darkMode ? "white" : "" }}>Internship</span>
+      <span>Experience</span>
+      <Company
+        link="https://www.volteomaritime.com/"
+        cName="Volteo Maritime"
+        list1="Presently Working"
+        list2="Tech Stack used HTML, CSS, JS, Typescript, ReactJS"
+        list3="Framework used Tailwind CSS."
+        list4="Working with brilliant team."
+        list5="More things to be explored..."
+        skill1={ReactJS}
+        skill2={HTML}
+        skill3={JS}
+        skill4={Tailwind}
+        skill5={TS}
+      />
+      <Company
+        link="https://www.digitalboxitin.com/"
+        cName="DigitalBoxItIn"
+        list1="Responsible for handling the Front-End of the Wesbite."
+        list2="Tech Stack used HTML, CSS, JS, ReactJS."
+        list3="Framework used Bootstrap."
+        list4="Learnt to Work in a Team."
+        list5="Learnt Time Management."
+        skill1={ReactJS}
+        skill2={HTML}
+        skill3={JS}
+        skill4={Bootstrap}
+        skill5={CSS}
+      />
     </div>
   );
 };

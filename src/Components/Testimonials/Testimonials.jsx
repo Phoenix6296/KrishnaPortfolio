8b5fa7c16b37ced8a26bacdc63f752ecb902345e
Krishna <krishnabiswakarmasc@gmail.com>
2022-10-19 11:29:39 +0530

    Added the designation of the recommenders

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -14,24 +14,28 @@ const Testimonials = () => {
     {
       img: Abdul,
       name: "Abdul Ghani",
+      designation: "Software Engineer @Volteo Maritime",
       review:
         "Krishna has outstanding work ethics, he is a quick learner and always thinks creatively. He actively contributed to the product. I was glad to have him in my team.",
     },
     {
       img: Mitrajit,
       name: "Mitrajit Chandra Chandra",
+      designation: "Backend Engineer @Listed Fans",
       review:
         "Krishna's commitment to his work is evident in the consistent high quality of his work. His ability to think deep gave us meaningful insight in unthought fields. He is a team player that's a pleasure to work with.",
     },
     {
       img: Kushankur,
       name: "Kushankur Das",
+      designation: "Advance App Software Engineer @Accenture",
       review:
         "Had a great time working  with Krishna Biswakarma on a same project. Got to learn great new skills. I must say he is very knowledgeable and helpful and got good leadership skills. Was really a pleasure working with such a teamate.",
     },
     {
       img: Sudeep,
       name: "Sudeep Deyondi",
+      designation: "Software Engineer @DigitalBoxItIn",
       review:
         "He has successfully undertaken and completed multiple projects and contributed significantly to success. His dedication to the team is quiet impressive. He is a talented and highly motivated developer with can-do attitude.",
     },
@@ -59,6 +63,7 @@ const Testimonials = () => {
               <div className="testimonial">
                 <img src={client.img} alt="" />
                 <span>{client.name}</span>
+                <span>{client.designation}</span>
                 <span>{client.review}</span>
               </div>
             </SwiperSlide>

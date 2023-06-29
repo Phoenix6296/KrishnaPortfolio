209836f45d8fdc02432f9e001879ad133adb08b3
Krishna Biswakarma <krishna@openinapp.com>
2023-06-29 11:46:56 +0530

    Updated the image in circle

--- src/Components/Works/Company/Company.jsx
+++ src/Components/Works/Company/Company.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import "./Company.css";
 import { motion } from "framer-motion";
 
-const Company = ({
-  skill1,
-  skill2,
-  skill3,
-  skill4,
-  skill5,
-  link,
-  companyName,
-  tasks,
-}) => {
-  
+const RotatingCircle = ({ className, background, children }) => (
+  <motion.div
+    animate={{ rotate: [360, 0, 360] }}
+    transition={{ duration: 20, type: "ease", yoyo: Infinity }}
+    className={`w-secCircle ${className}`}
+    style={{ background }}
+  >
+    {children}
+  </motion.div>
+);
+
+const Company = ({ link, companyName, tasks, skills }) => {
   return (
     <div className="works" id="Internship">
       <div className="awesome internship">
@@ -28,8 +29,7 @@ const Company = ({
         </span>
         <ul className="w-details">
           {tasks.map((task) => {
-            let heading = task.split(":")[0];
-            let content = task.split(":")[1];
+            let [heading, content] = task.split(":");
             return (
               <li key={heading}>
                 <span className="task_heading">{heading} :</span>
@@ -47,48 +47,19 @@ const Company = ({
             transition={{ duration: 20, type: "ease", yoyo: Infinity }}
             className="w-mainCircle"
           >
-            <motion.div
-              animate={{ rotate: [360, 0, 360] }}
-              transition={{ duration: 20, type: "ease", yoyo: Infinity }}
-              className="w-secCircle"
-              style={{ background: "#00d8ff" }}
-            >
-              {React.createElement(skill1, { color: "white", size: "4rem" })}
-            </motion.div>
-            <motion.div
-              animate={{ rotate: [360, 0, 360] }}
-              transition={{ duration: 20, type: "ease", yoyo: Infinity }}
-              className="w-secCircle"
-              style={{ background: "#D7492F" }}
-            >
-              <img src={skill2} alt="" width="500%" height="500%" />
-            </motion.div>
-            <motion.div
-              animate={{ rotate: [360, 0, 360] }}
-              transition={{ duration: 20, type: "ease", yoyo: Infinity }}
-              className="w-secCircle"
-              style={{ background: "#F7E04B" }}
-            >
-              {React.createElement(skill3, {
-                color: "black",
-                size: "4rem",
-                style: { background: "#F7E04B" },
-              })}
-            </motion.div>
-            <motion.div
-              animate={{ rotate: [360, 0, 360] }}
-              transition={{ duration: 20, type: "ease", yoyo: Infinity }}
-              className="w-secCircle"
-            >
-              <img src={skill4} alt="" width="450%" />
-            </motion.div>
-            <motion.div
-              animate={{ rotate: [360, 0, 360] }}
-              transition={{ duration: 20, type: "ease", yoyo: Infinity }}
-              className="w-secCircle"
-            >
-              <img src={skill5} alt="" width="500%" height="500%" />
-            </motion.div>
+            {skills.map((skill, index) => (
+              <RotatingCircle
+                key={index}
+                className={`circle-${index + 1}`}
+                background={skill.background}
+              >
+                {skill.type === "icon" ? (
+                  React.createElement(skill.component, skill.props)
+                ) : (
+                  <img src={skill.component} alt="" {...skill.props} />
+                )}
+              </RotatingCircle>
+            ))}
           </motion.div>
           <div className="w-backCircle blueCircle"></div>
           <div className="w-backCircle yellowCircle"></div>
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,6 +1,5 @@
 import "./Works.css";
 import React, { useContext } from "react";
-import { themeContext } from "../../Context";
 import Company from "./Company/Company";
 import CypressLogo from "../../Assets/Images/cypress_logo.webp";
 import MuiLogo from "../../Assets/Images/mui_logo.webp";
@@ -9,6 +8,11 @@ import JavaScriptIcon from "@iconscout/react-unicons/icons/uil-java-script";
 import BootstrapLogo from "../../Assets/Images/bootstrap_logo.webp";
 import CssLogo from "../../Assets/Images/css_logo.webp";
 import HtmlLogo from "../../Assets/Images/html_logo.webp";
+import NextJSIcon from "../../Assets/Images/nextjs_logo.webp";
+import TailwindLogo from "../../Assets/Images/tailwind_logo.webp";
+import JotaiLogo from "../../Assets/Images/jotai_logo.webp";
+import NodeJSLogo from "../../Assets/Images/node_logo.webp";
+import { themeContext } from "../../Context";
 
 const Works = () => {
   const theme = useContext(themeContext);
@@ -24,11 +28,35 @@ const Works = () => {
         "Email Sequencing: Coordinated with the growth team to implement an email sequencing system for personalized user engagement, achieving a 10% conversion rate with over 10000+ emails sent.",
         "Payment Integration: Integrated Stripe as a payment gateway, facilitating secure transactions for premium features and subscriptions.",
       ],
-      skill1: ReactIcon,
-      skill2: HtmlLogo,
-      skill3: JavaScriptIcon,
-      skill4: MuiLogo,
-      skill5: CypressLogo,
+      skills: [
+        {
+          type: "image",
+          component: NextJSIcon,
+          props: { color: "white", size: "4rem" },
+          background: "#00d8ff",
+        },
+        {
+          type: "image",
+          component: JotaiLogo,
+          props: { width: "500%", height: "500%" },
+        },
+        {
+          type: "icon",
+          component: JavaScriptIcon,
+          props: { color: "black", size: "4rem" },
+          background: "#F7E04B",
+        },
+        {
+          type: "image",
+          component: NodeJSLogo,
+          props: { width: "600%" },
+        },
+        {
+          type: "image",
+          component: TailwindLogo,
+          props: { width: "500%", height: "500%" },
+        },
+      ],
     },
     {
       link: "https://www.volteomaritime.com/",
@@ -38,11 +66,38 @@ const Works = () => {
         "Custom Signature Canvas: Developed a custom signature component utilizing ReactJS and Redux.",
         "End-to-End Testing: Conducted end-to-end testing using the Cypress framework, ensuring application reliability and performance.",
       ],
-      skill1: ReactIcon,
-      skill2: HtmlLogo,
-      skill3: JavaScriptIcon,
-      skill4: MuiLogo,
-      skill5: CypressLogo,
+      skills: [
+        {
+          type: "icon",
+          component: ReactIcon,
+          props: { color: "white", size: "4rem" },
+          background: "#00d8ff",
+        },
+        {
+          type: "image",
+          component: HtmlLogo,
+          props: { width: "500%", height: "500%" },
+          background: "#D7492F",
+        },
+        {
+          type: "icon",
+          component: JavaScriptIcon,
+          props: { color: "black", size: "4rem" },
+          background: "#F7E04B",
+        },
+        {
+          type: "image",
+          component: BootstrapLogo,
+          props: { width: "450%" },
+          background: "",
+        },
+        {
+          type: "image",
+          component: CypressLogo,
+          props: { width: "500%", height: "500%" },
+          background: "",
+        },
+      ],
     },
     {
       link: "https://www.digitalboxitin.com/",
@@ -52,11 +107,38 @@ const Works = () => {
         "Loading Time: Improved the loading time of the website using CDN and reducing the payload size of the webapp.",
         "Others: Worked on various Test Projects for Business.",
       ],
-      skill1: ReactIcon,
-      skill2: HtmlLogo,
-      skill3: JavaScriptIcon,
-      skill4: BootstrapLogo,
-      skill5: CssLogo,
+      skills: [
+        {
+          type: "icon",
+          component: ReactIcon,
+          props: { color: "white", size: "4rem" },
+          background: "#00d8ff",
+        },
+        {
+          type: "image",
+          component: HtmlLogo,
+          props: { width: "500%", height: "500%" },
+          background: "#D7492F",
+        },
+        {
+          type: "icon",
+          component: JavaScriptIcon,
+          props: { color: "black", size: "4rem" },
+          background: "#F7E04B",
+        },
+        {
+          type: "image",
+          component: MuiLogo,
+          props: { width: "450%" },
+          background: "",
+        },
+        {
+          type: "image",
+          component: CssLogo,
+          props: { width: "500%", height: "500%" },
+          background: "",
+        },
+      ],
     },
   ];
 
@@ -70,11 +152,7 @@ const Works = () => {
           link={company.link}
           companyName={company.companyName}
           tasks={company.tasks}
-          skill1={company.skill1}
-          skill2={company.skill2}
-          skill3={company.skill3}
-          skill4={company.skill4}
-          skill5={company.skill5}
+          skills={company.skills}
         />
       ))}
     </div>
